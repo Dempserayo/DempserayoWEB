afb3595080ecb80e9529c2ad9d80479db1c2fd61
Raye <160432415+Dempserayo@users.noreply.github.com>
2026-03-05 23:37:00 -0500

    Add react-icons dependency and update Home component layout

--- mi-proyecto/app/page.tsx
+++ mi-proyecto/app/page.tsx
@@ -1,8 +1,14 @@
+import Navbar from "./components/constants/navbar";
 
 export default function Home() {
   return (
-  <div className="w-full h-screen bg-white flex flex-row justify-center items-center gap-4 p-10">
-      <div className="w-full h-full border border-black"></div>
+  <div className="w-full h-auto bg-neutral-50 backdrop-blur-2xl flex flex-col items-center gap-4">
+    <Navbar/>
+    <section className="w-full h-auto flex flex-col  justify-center items-center">
+      <div className="w-full h-80 bg-slate-800"></div>
+      <div className="w-full h-screen bg-slate-200"></div>
+    </section>
+    <div className="w-full h-80 "></div>
   </div>
   );
 }
