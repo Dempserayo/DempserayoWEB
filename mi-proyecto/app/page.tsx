56fc38cc3493697322bb14f8caf1382b2c9a2b77
Raye <160432415+Dempserayo@users.noreply.github.com>
2026-03-05 20:37:43 -0500

    Implement initial project structure and setup

---
+++ mi-proyecto/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+  <div className="w-full h-screen bg-white flex flex-row justify-center items-center gap-4 p-10">
+      <div className="w-full h-full border border-black"></div>
+  </div>
+  );
+}
