4e4217b26e414d86685fa033b018f573136f486b
Raye <160432415+Dempserayo@users.noreply.github.com>
2026-03-06 15:27:00 -0500

    Enhance Banner component layout with user profile and additional info sections

--- mi-proyecto/app/components/common/banner/index.tsx
+++ mi-proyecto/app/components/common/banner/index.tsx
@@ -10,20 +10,47 @@ export default function Banner() {
         </section>
   
         <section className="w-full flex justify-center items-center p-20 text-slate-500 font-thin text-xs">
-            <div className="w-full max-w-7xl flex flex-row gap-4">
-              <>
-                <section className="w-auto flex items-center gap-4">
-                  <Link href='/' className="w-10 h-10 rounded-full bg-slate-500 hover:bg-slate-400 transition-all duration-500 text-white flex items-center justify-center">
-                      <span className="uppercase">ma</span>
-                  </Link>
+            <section className="w-full max-w-7xl flex flex-row gap-4">
+                
+                <section className="w-full flex items-center justify-between">
+                    {/* Perfil  */}
+                    <div className="w-auto flex justify-center items-center gap-4">
+                      <Link href='https://github.com/Dempserayo' className="w-10 h-10 bg-slate-500 hover:bg-slate-400 transition-all duration-500 text-white flex items-center justify-center">
+                          <span className="uppercase">ma</span>
+                      </Link>
 
-                  <div>
-                    <p className="text-xs font-thin">Miguel Angel J P</p>
-                    <p className="text-slate-500/50 text-xs font-thin">Dempserayo · Creator & Maintainer</p>
-                  </div>
+                      <div>
+                        <p className="text-xs font-thin">Miguel Angel J P</p>
+                        <p className="text-slate-500/50 text-xs font-thin">Dempserayo · Creator & Maintainer</p>
+                      </div>
+                    </div>
+
+
+                    {/* Info  */}
+                    <div className="w-auto flex justify-center items-center gap-4">
+                      <>
+                        <div>
+                          <p className="text-xs font-thin">+ 2 / 150 </p>
+                          <p className="text-slate-500/50 text-xs font-thin">Components</p>
+                        </div>
+                      </>
+                      <>
+                        <div>
+                          <p className="text-xs font-thin">98% / 100%</p>
+                          <p className="text-slate-500/50 text-xs font-thin">Accessibility Score</p>
+                        </div>
+                      </>
+                      <>
+                        <div>
+                          <p className="text-xs font-thin"> 1 / 1</p>
+                          <p className="text-slate-500/50 text-xs font-thin">Development Team</p>
+                        </div>
+                      </>
+        
+                    </div>
                 </section>
-              </>
-            </div>
+            
+            </section>
         </section>
       </div>
     );
