7a5ff8a8668ece7da49dafd897a60a0132780fc9
Raye <160432415+Dempserayo@users.noreply.github.com>
2026-03-06 23:23:18 -0500

    Refactor ViewComponents and ViewDownload components to improve layout and styling; adjust padding and text colors for better readability

--- mi-proyecto/app/components/common/viewComponents/index.tsx
+++ mi-proyecto/app/components/common/viewComponents/index.tsx
@@ -3,7 +3,7 @@ import { FiArrowUpRight } from "react-icons/fi";
 export default function ViewComponents() {
     return(
       <>
-      <section className="w-full h-screen flex flex-col justify-center items-center gap-2">
+      <section className="w-full h-auto flex flex-col justify-center items-center gap-2 p-20">
             {/* Basico 1 */}
             <section className="w-full max-w-7xl h-auto font-thin text-xs text-slate-500  flex flex-row gap-2">
                 <>
@@ -32,16 +32,9 @@ export default function ViewComponents() {
                             <p className="uppercase">Banners</p>
                             <FiArrowUpRight />
                         </div>
-                        <div className="w-80 h-80 flex flex-row justify-between bg-transparent border border-slate-200 p-8 cursor-pointer hover:bg-rose-200/50 hover:border-rose-500 hover:text-rose-500 transition-all duration-500">
-                            <p className="uppercase">Banners</p>
-                            <FiArrowUpRight />
-                        </div>
-                
                     </section>
                 </>
             </section>
-
-           
       </section>
       </>
     )
--- mi-proyecto/app/components/common/viewDownload/index.tsx
+++ mi-proyecto/app/components/common/viewDownload/index.tsx
@@ -4,25 +4,30 @@ import { FiCopy } from "react-icons/fi";
 
 export default function ViewDownload() {
     return(
-        <section className="w-full h-96 bg-linear-to-br from-slate-800 via-slate-500 to-slate-500 flex flex-col justify-center items-center overflow-hidden gap-6">
+        <section className="w-full h-96 bg-linear-to-br from-slate-800 via-slate-500 to-slate-500 flex flex-col justify-center items-center overflow-hidden gap-20">
             <section className="w-full max-w-7xl flex flex-col gap-4">
-                <p className="text-slate-300 text-xs uppercase font-thin">
+                <p className="text-slate-200 text-xs uppercase font-thin">
                     Get started in seconds
                 </p>
 
                 <div className="w-full flex items-center group gap-2">
                     <div className="w-full h-20 flex items-center p-10 bg-slate-900/80 ">
-                        <span className="text-slate-300 font-mono text-sm tracking-wide">
+                        <span className="text-slate-200 font-mono text-sm tracking-wide">
                             <span className="text-slate-500 select-none">$ </span>
                             npm i dempserayo-ui
                         </span>
                     </div>
 
-                    <button onClick={() => { navigator.clipboard.writeText("npm i dempserayo-ui");}} className="bg-slate-700 text-slate-500 border border-slate-500 hover:border-rose-500 hover:bg-rose-200/50 hover:text-rose-500  w-20 h-20 flex justify-center items-center transition-all duration-500 group cursor-pointer" title="Copy to clipboard">
-                    <FiCopy />                
+                    <button 
+                        onClick={() => { navigator.clipboard.writeText("npm i dempserayo-ui");}} 
+                        className="bg-slate-700 text-slate-500 border border-slate-500 hover:border-rose-500 hover:bg-rose-200/50 hover:text-rose-500  w-20 h-20 flex justify-center items-center transition-all duration-500 group cursor-pointer" 
+                        title="Copy to clipboard"
+                    >
+                        <FiCopy />                
                     </button>
                 </div>
             </section>
+
         </section>
     )
 }
