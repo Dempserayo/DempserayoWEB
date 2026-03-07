8ac5ce0c31a149af10a5abf2139d875f83bbc571
Raye <160432415+Dempserayo@users.noreply.github.com>
2026-03-06 22:58:50 -0500

    Update ViewComponents to change cursor style to pointer for interactive elements and add a new Banners section

--- mi-proyecto/app/components/common/viewComponents/index.tsx
+++ mi-proyecto/app/components/common/viewComponents/index.tsx
@@ -8,7 +8,7 @@ export default function ViewComponents() {
             <section className="w-full max-w-7xl h-auto font-thin text-xs text-slate-500  flex flex-row gap-2">
                 <>
                     <section className="w-full h-auto flex flex-row gap-2">
-                        <div className="w-full h-80 flex flex-row justify-between bg-transparent border border-slate-200 p-8 cursor-none hover:bg-rose-200/50 hover:border-rose-500 hover:text-rose-500 transition-all duration-500">
+                        <div className="w-full h-80 flex flex-row justify-between bg-transparent border border-slate-200 p-8 cursor-pointer hover:bg-rose-200/50 hover:border-rose-500 hover:text-rose-500 transition-all duration-500">
                             <p className="uppercase">Layout</p>
                             <FiArrowUpRight />
                         </div>
@@ -16,7 +16,7 @@ export default function ViewComponents() {
                 </>
                 <>
                     <section className="w-full h-auto flex flex-row gap-2">
-                        <div className="w-full h-80 flex flex-row justify-between bg-transparent border border-slate-200 p-8 cursor-none hover:bg-rose-200/50 hover:border-rose-500 hover:text-rose-500 transition-all duration-500">
+                        <div className="w-full h-80 flex flex-row justify-between bg-transparent border border-slate-200 p-8 cursor-pointer hover:bg-rose-200/50 hover:border-rose-500 hover:text-rose-500 transition-all duration-500">
                             <p className="uppercase">Footers</p>
                             <FiArrowUpRight />
                         </div>
@@ -28,10 +28,15 @@ export default function ViewComponents() {
             <section className="w-full max-w-7xl h-auto font-thin text-xs text-slate-500  flex flex-row gap-2">
                 <>
                     <section className="w-full h-auto flex flex-row gap-2">
-                        <div className="w-full h-80 flex flex-row justify-between bg-transparent border border-slate-200 p-8 cursor-none hover:bg-rose-200/50 hover:border-rose-500 hover:text-rose-500 transition-all duration-500">
+                        <div className="w-full h-80 flex flex-row justify-between bg-transparent border border-slate-200 p-8 cursor-pointer hover:bg-rose-200/50 hover:border-rose-500 hover:text-rose-500 transition-all duration-500">
                             <p className="uppercase">Banners</p>
                             <FiArrowUpRight />
                         </div>
+                        <div className="w-80 h-80 flex flex-row justify-between bg-transparent border border-slate-200 p-8 cursor-pointer hover:bg-rose-200/50 hover:border-rose-500 hover:text-rose-500 transition-all duration-500">
+                            <p className="uppercase">Banners</p>
+                            <FiArrowUpRight />
+                        </div>
+                
                     </section>
                 </>
             </section>
