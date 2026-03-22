81f8fcfcf09a312588ee15a2f32a3d0c6e232d23
Raye <160432415+Dempserayo@users.noreply.github.com>
2026-03-21 23:34:48 -0500

    First preview of the feature for changing the page theme: dark mode and light mode

--- app/components/constants/navbar/index.tsx
+++ app/components/constants/navbar/index.tsx
@@ -3,7 +3,7 @@ import { FiSearch } from "react-icons/fi";
 
 export default function Navbar() {
     return(
-        <nav className="w-full h-auto py-20 px-10   flex flex-row justify-center items-center text-xs font-thin ">
+        <nav className="w-full h-auto py-20 px-10 flex flex-row justify-center items-center text-xs font-thin ">
           <section className="w-full max-w-7xl flex flex-row  justify-between items-center">
             <section className="flex flex-row  items-center gap-4">
               <Link href='/' className="font-black text-xl text-slate-500 hover:text-rose-500 transition-all duration-500">D-UI</Link>
